98e08a927c9f2760e97ea00f451d718873c66ed8
Shiny-Sub <239194147@qq.com>
2018-11-04 15:28:35 +0800

    python.py

---
+++ Python/python01.py
@@ -0,0 +1,51 @@
+一.
+python:
+bin();
+oct();
+hex();//二,八,十六进制转换
+complex 复数
+计算机中float表示是有精度限制
+%取模运算	7%3 = 1;
+**求幂运算	2**10 = 1024;
+//向下取整
+python中等号左边必须是变量;
+x = 1;
+x += 1;
+x = 2;
+< > <= >= != ==	(比较操作)(非赋值)
+逻辑运算  and or not  (产生 bool 类型的逻辑运算)
+身份运算符 is 
+成员运算符 in
+位运算符
+二.
+字符串
+用双引号包裹的单引号不用转义;
+ord('A')
+chr(97)
+字符编码转换
+r'\\\n'输出r'\\\\\\n'
+统一Unicode编码
+greeting = 'abcde'
+len(greeting) = 5
+greeting[1] = b;
+greeting[-5] = a;
+greeting[1:4] = 'bcd'
+截取字符串  前闭后开
+
+'hello' * 3	'hellohellohello'
+'+'字符串拼接
+'in' 判断是否是字串
+获取字符串长度len()
+startswith()
+endswith()
+'hello'.startswith('h')
+'生命.宇宙及一切的终极答案是{}'.format(32)
+'生命.宇宙及一切的终极答案是32'
+a = 2
+b = 3
+c = 7
+'{0}, {2} and {1}'.format(a,b,c)
+'2, 7 and 3'
+
+字符串2
+条件语句
