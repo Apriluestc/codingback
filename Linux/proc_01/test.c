3fb8c241780a29a61c759f383df0c26df4a75e2c
Shiny-Sub <239194147@qq.com>
2018-11-30 20:52:34 +0800

    进程初识

---
+++ Linux/proc_01/test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I am boy\n");
+    printf("Hello world\n");
+    return 0;
+}
+
---
+++ Linux/proc_01/tmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    int ret = fork();
+    if(ret < 0)
+    {
+        perror("fork");
+        exit(0);
+    }
+    else if(ret == 0)
+    {
+        printf("I am child process!!!\n");
+    }
+    else
+    {
+        printf("I am parent process!!!\n");
+    }
+    */
+    printf("Hello world\n");
+    return 0;
+}
+
