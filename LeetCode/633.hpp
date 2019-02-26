e4d8c1fb13a5f80d3675ef7a008d21c82288fd99
Shiny-Sub <239194147@qq.com>
2019-02-26 14:24:40 +0800

    Leetcode：633

---
+++ LeetCode/633.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+class 633
+{
+public:
+    bool judgeSquareSum(int c)
+    {
+        long long left = 0, right = sqrt(c);
+        while(left <= right)
+        {
+            long long sum = left * left + right * right;
+            if(sum == c)
+                return true;
+            else if(sum < c)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+private:
+
+};
+
